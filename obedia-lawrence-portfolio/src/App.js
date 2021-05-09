8f3cc3ec018b89f3bb0e2ac4e9575a43205b1883
Obedia Lawrence <omlawrence24@gmail.com>
2021-05-09 14:01:50 -0400

    header complete

--- obedia-lawrence-portfolio/src/App.js
+++ obedia-lawrence-portfolio/src/App.js
@@ -1,12 +1,39 @@
 // import React from "react";
 // import Portfolio from "./page/portfolio";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from "react-particles-js"
 import NavBar from "./components/Nav";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
-  return <NavBar />;
-
+  return (
+  <>
+  <Particles
+  className="particles-canvas"
+  params={{
+    particles: {
+      number: {
+       value: 30,
+       density: {
+         enable: true,
+         value_area: 900
+       }
+      },
+      shape:{
+        type: "circle",
+        stroke: {
+          width: 6,
+          color: "#f9ab00"
+        }
+      }
+    }
+  }}
+  />
+  <NavBar />;
+ <Header />;
+  </>
+  );
 }
 
 export default App;
