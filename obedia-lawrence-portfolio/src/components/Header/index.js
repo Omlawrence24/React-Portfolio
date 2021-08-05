ca7eccff6fdca7068c6edbcd32baf19809df810f
Obedia Lawrence <omlawrence24@gmail.com>
2021-08-05 12:35:22 -0400

    updating4

--- obedia-lawrence-portfolio/src/components/Header/index.js
+++ obedia-lawrence-portfolio/src/components/Header/index.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div id="header">
             <div className="header-wraper">
                 <div className="main-info">
-
+                
                     <h1> Obedia "bebe" Lawrence </h1>
                     <Typed
                         className="typed-text"
