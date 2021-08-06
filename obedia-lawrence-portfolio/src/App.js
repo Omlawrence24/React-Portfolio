39b276099820d00b235ca479a695fbe5291f29b2
New User2 <kiser396@gmail.com>
2021-08-06 11:24:16 -0400

    updating

--- obedia-lawrence-portfolio/src/App.js
+++ obedia-lawrence-portfolio/src/App.js
@@ -1,50 +1,23 @@
-// import React from "react";
-// import Portfolio from "./page/portfolio";
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Particles from "react-particles-js"
 import NavBar from "./components/Nav";
 import Header from "./components/Header";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Services from "./components/Services";
 import Projects from "./components/Projects";
-// import Contacts from "./components/Contacts";
 
 import "./App.css";
 
 function App() {
   return (
-  <>
-  <Particles
-  className="particles-canvas"
-  params={{
-    particles: {
-      number: {
-       value: 30,
-       density: {
-         enable: true,
-         value_area: 900
-       }
-      },
-      shape:{
-        type: "circle",
-        stroke: {
-          width: 6,
-          color: "#f9ab00"
-        }
-      }
-    }
-  }}
-  />
-  <NavBar />;
- <Header />;
- < About/>;
- <Services/>;
- <Projects/>;
- {/* <Contacts/>; */}
- <Footer/>;
-  </>
+    <div>
+      <NavBar />;
+      <Header />;
+      <About />;
+      <Services />;
+      <Projects />;
+      <Footer />;
+    </div>
   );
 }
 
--- obedia-lawrence-portfolio/src/components/Header/index.js
+++ obedia-lawrence-portfolio/src/components/Header/index.js
@@ -1,23 +1,44 @@
 import React from "react"
 import Typed from "react-typed";
+import Particles from "react-particles-js";
+
+
 const Header = () => {
     return (
-        <div id="header">
-            <div className="header-wraper">
+
+        <div id="main">
+            <Particles className="particles-canvas"
+                params={{
+                    particles: {
+                        number: {
+                            value: 30,
+                            density: {
+                                enable: true,
+                                value_area: 900
+                            }
+                        },
+                        shape: {
+                            type: "circle",
+                            stroke: {
+                                width: 6,
+                                color: "#f9ab00"
+                            }
+                        }
+                    }
+                }} />
+            <div className="header">
                 <div className="main-info">
-                
-                    <h1> Obedia "bebe" Lawrence </h1>
-                    <Typed
-                        className="typed-text"
-                        strings={["Web Design", "Web Development", "Relational DataBase", "UI/UX"]}
-                        typeSpeed={40}
-                        backSpeed={60}
-                        loop
-                    />
-                    {/* / */}
+                    <div class="centered">
+                        <h1> Obedia "bebe" Lawrence </h1>
+                        <Typed
+                            className="typed-text"
+                            strings={["Web Design", "Web Development", "Relational DataBase", "UI/UX"]}
+                            typeSpeed={40}
+                            backSpeed={60}
+                            loop
+                        />
+                    </div>
                 </div>
-
-
             </div>
         </div>
     )
